b8c2ad733e673b40a8dbf226fe522b8ec770db77
ajaysinghrajput9370 <ajaysinghrajput9370@gmail.com>
2026-08-12 20:25:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 
+from database import init_db
+
+
 app = Flask(__name__)
+
+
+# Database tables create
+init_db()
+
 
 @app.route("/")
 def home():
@@ -9,7 +17,10 @@ def home():
 
 @app.route("/prompt/<int:prompt_id>")
 def prompt_detail(prompt_id):
-    return render_template("prompt.html", prompt_id=prompt_id)
+    return render_template(
+        "prompt.html",
+        prompt_id=prompt_id
+    )
 
 
 @app.route("/admin")
